00ff5dcc42f240c4c6f8002260e9d65113e3d05c
Pavan Sonje <70367808+SonjePavan@users.noreply.github.com>
2020-11-29 14:57:31 +0530

    Update youtube_download.py

--- youtube_download.py
+++ youtube_download.py
@@ -7,15 +7,14 @@ videos=yt.streams.filter(progressive=True, file_extension='mp4')
 
 i=1
 for stream in videos:
-    # if (stream.is_progressive ==True):
-        print(str(i) + " " + str(stream))
-        i+= 1
+    print(str(i) + " " + str(stream))
+    i+= 1
 
 choice=int(input("Select Your Choice :"))
 video=videos[choice-1]
 print("Download Started")
-video.download("E://") 
-print("Downloaded")
+video.download("E://") #Use Custom File Path.
+print("Download Completed")
 
 
 
